b0896f569c10d7f56a0d22b26bc0beb75657c3c2
Josh Owens <me@jowens.dev>
2021-11-14 21:38:26 -0600

    adding headless ui components

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,12 +1,26 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref } from 'vue'
+  import { Switch } from '@headlessui/vue'
+
+  const enabled = ref(false)
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <div class="container mx-auto">
+    <Switch
+      v-model="enabled"
+      :class="enabled ? 'bg-green-500' : 'bg-gray-300'"
+      class="relative inline-flex items-center h-6 rounded-full w-11"
+    >
+      <span class="sr-only">Enable notifications</span>
+      <span
+        :class="enabled ? 'translate-x-6' : 'translate-x-1'"
+        class="inline-block w-4 h-4 transform bg-white rounded-full"
+      />
+    </Switch>
+  </div>
 </template>
 
 <style>
