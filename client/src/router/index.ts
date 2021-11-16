50628f09a83acf3427a5df7d80f059e26c22ff16
Josh Owens <me@jowens.dev>
2021-11-15 21:10:38 -0600

    shelling out views dir

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,9 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HelloWorld from '@/components/HelloWorld.vue';
+import Home from '@/views/Home.vue';
+import Login from '@/views/Login.vue';
 
 
 const routes = [
-    { path: '/', component: HelloWorld },
+    { path: '/', component: Home },
+    { path: '/login', component: Login },
 ];
 
 const router = createRouter({
