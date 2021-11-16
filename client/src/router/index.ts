6765d4e9e91256786158ea2fe2516144a4bf904f
Josh Owens <me@jowens.dev>
2021-11-15 21:15:20 -0600

    removing ununsed import

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -1,5 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HelloWorld from '@/components/HelloWorld.vue';
 import Home from '@/views/Home.vue';
 import Login from '@/views/Login.vue';
 
