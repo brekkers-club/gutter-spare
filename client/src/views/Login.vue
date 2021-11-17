09fa3d8d1ea7dc27b9c97862adc018183315d9fa
Josh Owens <me@jowens.dev>
2021-11-16 21:15:15 -0600

    setting up axios to make requests

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,5 +1,19 @@
+<script>
+import { defineComponent, onBeforeMount } from "@vue/runtime-core";
+import axios from 'axios';
+
+export default defineComponent({
+  setup() {
+    onBeforeMount(() => {
+        axios.get('http://localhost:8888/sanctum/csrf-cookie')
+            .then((response) => {
+                console.log({ response });
+            });
+    });
+  },
+});
+</script>
+
 <template>
-    <div>
-        Login
-    </div>
+  <div>Login</div>
 </template>
