b5983e43a1145792c02593e810558f3a2b847dca
Josh Owens <me@jowens.dev>
2021-11-20 12:25:41 -0600

    Hooking up login form to login route

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -4,13 +4,32 @@ import { AuthProvider } from "@/providers/auth";
 
 export default defineComponent({
   setup() {
-    const auth: AuthProvider = inject("auth");
+    const auth = inject<AuthProvider>("auth");
 
     onBeforeMount(() => {
-      auth.csrf().then((response) => {
+      auth?.csrf().then((response) => {
         console.log({ response });
       });
     });
+
+    return {
+      auth,
+    }
+  },
+  data() {
+    return {
+      credentials: {
+        username: '',
+        password: '',
+      }
+    }
+  },
+  methods: {
+    login() {
+      this.auth?.login(this.credentials)
+        .then((response) => console.log({ response }))
+        .catch((error) => console.error({ error }));
+    }
   },
 });
 </script>
@@ -35,11 +54,11 @@ export default defineComponent({
         <div class="rounded-md shadow-sm -space-y-px">
           <div>
             <label for="email-address" class="sr-only">Email address</label>
-            <input id="email-address" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
+            <input id="email-address" v-model="credentials.username" name="email" type="email" autocomplete="email" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address">
           </div>
           <div>
             <label for="password" class="sr-only">Password</label>
-            <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
+            <input id="password" v-model="credentials.password" name="password" type="password" autocomplete="current-password" required class="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password">
           </div>
         </div>
 
@@ -59,7 +78,7 @@ export default defineComponent({
         </div>
 
         <div>
-          <button type="submit" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+          <button type="button" @click="() => login()" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
             <span class="absolute left-0 inset-y-0 flex items-center pl-3">
               <!-- Heroicon name: solid/lock-closed -->
               <svg class="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
