563b0986f4b5603cff08f29d2cc586a59177c363
Josh Owens <me@jowens.dev>
2021-11-23 07:15:34 -0600

    moving store into its own file

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,29 +1,8 @@
 import { createApp } from 'vue'
-import { createStore } from 'vuex'
 import './index.css';
 import App from './App.vue';
 import router from './router';
-
-interface VuexState {
-  loggedIn: boolean,
-}
-
-const store = createStore({
-  state (): VuexState {
-    return {
-      loggedIn: false,
-    }
-  },
-  mutations: {
-    login(state: VuexState) {
-      state.loggedIn = true;
-      console.log({ state });
-    },
-    logout(state: VuexState) {
-     state.loggedIn = false;
-    }
-  }
-});
+import store from './store';
 
 createApp(App)
     .use(router)
