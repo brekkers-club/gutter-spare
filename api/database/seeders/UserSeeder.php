4ccf1239baae4ca898faff54d7969a0945ee5340
Josh Owens <me@jowens.dev>
2021-11-22 18:54:12 -0600

    adding basic user seeder

---
+++ api/database/seeders/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = new User();
+        $user->email = 'league@bowler.com';
+        $user->name = 'League Bowler';
+        $user->password = Hash::make('lane1234');
+        $user->save();
+    }
+}
